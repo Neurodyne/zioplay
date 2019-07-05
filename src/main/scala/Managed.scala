8fbefc22127a4b7e5f4a8c8c4823074936bddb5c
tampler <socnetfpga@gmail.com>
2019-07-05 13:15:48 +0300

    Added zmanaged

---
+++ src/main/scala/Managed.scala
@@ -0,0 +1,17 @@
+package hello
+
+import zio.{ App, ZIO, ZManaged }
+import zio.console.{putStrLn, getStrLn, Console}
+
+object Managed extends App {
+
+  def run(args: List[String]) =
+    myAppLogic.fold(_ => 1, _ => 0)
+
+    
+  val zManagedResource: ZManaged[Console, Nothing, Unit] = ZManaged.make(putStrLn("acquiring"))(_ => putStrLn("releasing"))
+  val zUsedResource: ZIO[Console, Nothing, Unit] = zManagedResource.use { _ => putStrLn("running") }
+  
+  val myAppLogic = zUsedResource
+
+}
